f49044d5416b6f0a45460100481e36864cb4da06
MaryBaptista <_marybaptistanwachukwu@gmail.com>
2023-02-05 11:51:13 +0000

    The mandatory task

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
-'''
-Locked box challenge
-'''
+"""This file contains the canUnloackAll function
+"""
 
 
 def canUnlockAll(boxes):
-    '''
-    Check boxes for key to unlock the next box
-    '''
-    if len(boxes[0]) == 0:
-        return (False)
-    keys = {0}
-    size = len(boxes)
-    visited = {0}
-    keys = keys.union(boxes[0])
-    while size > 0:
-        for i in keys:
-            if i in visited:
-                continue
-            keys = keys.union(boxes[i])
-            visited.add(i)
-        size -= 1
-    return len(keys) == len(boxes)
+    """This function checks if all the boxes can be opened"""
+
+    visited = [False] * len(boxes)
+    visited[0] = True
+    queue = [0]
+    while queue:
+        box = queue.pop(0)
+        for key in boxes[box]:
+            if key < len(boxes) and not visited[key]:
+                visited[key] = True
+                queue.append(key)
+    return all(visited)
